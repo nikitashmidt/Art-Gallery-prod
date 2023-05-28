f5c13aa52cc640faeaa4598c6300307e389a4dc6
Nikita Shmidt <71144807+nikitashmidt@users.noreply.github.com>
2023-05-28 11:33:32 +0600

    Add files via upload

---
+++ static/js/377.0534232f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[377],{7377:function(p,a,e){e.r(a);var u=e(1346);a.default=u.Z}}]);
+//# sourceMappingURL=377.0534232f.chunk.js.map
